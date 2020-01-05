f670b159cbe51b0109f0e65bc46e071448a875ed
codekoko <18290060679@163.com>
2020-01-05 10:24:29 +0800

    简单保存

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,30 +1,93 @@
 <template>
   <div id='home'>
+    <!-- 顶部导航栏 -->
     <nav-bar class="home-nav">
       <div slot='center'>购物街</div>
     </nav-bar>
-    <home-swiper :banners='banners'></home-swiper>
-    <home-recommend-view :recommends='recommends'></home-recommend-view>
+    <!-- 轮播 -->
+    <home-swiper :banners='banners' />
+    <!-- 推荐 -->
+    <home-recommend-view :recommends='recommends' />
+    <!-- 流行 -->
+    <feature-view />
+    <!-- 选项卡 -->
+    <tab-control
+      class="tab-control"
+      :title='["流行","新款","精选"]'
+    />
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+      <li>列表10</li>
+    </ul>
+
   </div>
 </template>
 <script>
+//全局公共组件--------------------------------------------
 import NavBar from "components/common/navbar/NavBar";
+
+//项目内公共组件--------------------------------------------
+import TabControl from "components/content/tabControl/TabControl";
+
+//子组件--------------------------------------------
 import HomeSwiper from "./childComps/HomeSwiper";
 import HomeRecommendView from "./childComps/HomeRecommendView";
+import FeatureView from "./childComps/FeatureView";
 
+//方法数据--------------------------------------------
 import { getHomeMultidata } from "network/home";
+
 export default {
   name: "Home",
   components: {
     NavBar,
+    TabControl,
     HomeSwiper,
-    HomeRecommendView
+    HomeRecommendView,
+    FeatureView
   },
   //保存获取的数据
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        news: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+        //160集
+      }
     };
   },
 
@@ -41,8 +104,22 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
+
 .home-nav {
   background-color: var(--color-tint);
   color: #ffffff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 10;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
